b7b24cfa265ae3cb324e7b419bdda91899b55188
jeongchaeuk <jeongchaeuk@gmail.com>
2023-11-23 11:03:31 +0900

    Start !

---
+++ README.md
@@ -0,0 +1,13 @@
+# ASCII Pixel Art
+
+## Source from:
+
+github: https://github.com/StanislavPetrovV/ASCII-art-PIXEL-art.git
+
+youtube: https://youtu.be/eLfRSAgXNZU?si=YHY_zVYNzwhrm8e1
+
+## Requires packages
+
+- pygame-ce
+- opencv-python
+- numba (caution! this required python < 3.12)
---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame as pg
+import cv2
+
+
+class ArtConvert:
+    def __init__(self):
+        pass
+
+    def get_image(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def run(self):
+        pass
+
+
+if __name__ == '__main__':
+    app = ArtConvert()
+    app.run()
