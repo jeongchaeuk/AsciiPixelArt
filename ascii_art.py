cf5456aa444515cf9145ee47cead527a7fd98467
jeongchaeuk <jeongchaeuk@gmail.com>
2023-11-23 14:29:27 +0900

    Show ascii art.

--- ascii_art.py
+++ ascii_art.py
@@ -3,7 +3,7 @@ import cv2
 
 
 class ArtConvert:
-    def __init__(self, path='img/car.jpg'):
+    def __init__(self, path='img/car.jpg', font_size=12):
         pg.init()
         self.path = path
         self.cv2_image = None
@@ -13,11 +13,30 @@ class ArtConvert:
         self.surface = pg.display.set_mode(self.RES, vsync=1)
         self.clock = pg.time.Clock()
 
+        self.ASCII_CHARS = ' .",:;!~+-xmo*#W&8@'
+        self.ASCII_COEFF = 255 // (len(self.ASCII_CHARS) - 1)
+        self.font = pg.font.SysFont('d2coding', font_size, bold=True)
+        self.CHAR_STEP = int(font_size * .6)
+        self.RENDERED_ASCII_CHARS = [self.font.render(char, False, 'white')
+                                     for char in self.ASCII_CHARS]
+        for index, surf in enumerate(self.RENDERED_ASCII_CHARS):
+            print(self.ASCII_CHARS[index], surf.get_size())
+
+    def draw_converted_image(self):
+        char_indices = self.image // self.ASCII_COEFF
+        for x in range(0, self.WIDTH, self.CHAR_STEP):
+            for y in range(0, self.HEIGHT, self.CHAR_STEP):
+                char_index = char_indices[x, y]
+                if char_index:
+                    self.surface.blit(self.RENDERED_ASCII_CHARS[char_index],
+                                      (x, y))
+
+
     def get_image(self):
         self.cv2_image = cv2.imread(self.path)  # numpy array
         transposed_image = cv2.transpose(self.cv2_image)
-        rgb_image = cv2.cvtColor(transposed_image, cv2.COLOR_RGB2BGR)
-        return rgb_image
+        gray_image = cv2.cvtColor(transposed_image, cv2.COLOR_BGR2GRAY)
+        return gray_image
 
     def draw_cv2_image(self):
         resized_cv2_image = cv2.resize(self.cv2_image, None, fx=0.5, fy=0.5,
@@ -25,8 +44,9 @@ class ArtConvert:
         cv2.imshow('opencv', resized_cv2_image)
 
     def draw(self):
-        pg.surfarray.blit_array(self.surface, self.image)
         self.draw_cv2_image()
+        self.surface.fill('black')
+        self.draw_converted_image()
 
     def run(self):
         while True:
